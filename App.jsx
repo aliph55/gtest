9f76f8ca4b3901c8e51e20a3669faaee3e6028da
Ali Pourhassan <67645398+aliph55@users.noreply.github.com>
2025-05-11 19:03:03 +0300

    reanimated-config

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import {View, Button, NativeModules} from 'react-native';
 
@@ -27,13 +28,38 @@ const App = () => {
       alert(`Ödüllü reklam yüklenemedi: ${error.message}`);
     }
   };
+=======
+import {View, Text} from 'react-native';
+import {createStaticNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+>>>>>>> Stashed changes
 
+function HomeScreen() {
   return (
+<<<<<<< Updated upstream
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Button title="Geçiş Reklamını Göster" onPress={showInterstitialAd} />
       <Button title="Ödüllü Reklamı Göster" onPress={showRewardedAd} />
+=======
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+>>>>>>> Stashed changes
     </View>
   );
-};
+}
 
+<<<<<<< Updated upstream
 export default App;
+=======
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
+>>>>>>> Stashed changes
